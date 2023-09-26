a26d380e138231d8d185ee983baeb43efe046da7
Mohamed Tarek <104282801+motarekk@users.noreply.github.com>
2023-09-26 11:16:13 +0300

    Update rot-bruteforcer.py

--- rot-bruteforcer.py
+++ rot-bruteforcer.py
@@ -1,73 +1,39 @@
 '''
 Caesar/ROT Cipher Brute-Forcer
-By: Mohamed Tarek
-GitHub: https://github.com/motarekk
-LinkedIn: https://www.linkedin.com/in/mohamed-tarek-159a821ba/
+Author: motarek
 '''
 import sys
 
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 #---- INPUT ----#
+ciphertext = ''
 
-#take input from user
-cipher_text = ''
-
+# take input from user
 for arg in ' '.join(sys.argv[1:]):
-    cipher_text  = cipher_text + arg
+    ciphertext += arg
 
-cipher_text_arr = [''] * len(cipher_text)
-    
-for i in range(len(cipher_text)):
-    cipher_text_arr[i] = cipher_text[i]
-
-#----------------------------------------------#
 #---- PROCESS ----#
+def caesar_decrypt(ciphertext, key): # input
+    # check key is an integer and is > 0
+    # assert --> make sure
+    assert key > 0
 
-#decipher function
-def decipher(key, cipher_text):
-    alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    ciphertext = ciphertext.upper()
+    plaintext = ''
 
-    '''
-    << Example >>
-    key = 3
-    len(alphabet) = 26
-    shiftedArr = [4-25, 0-3]
-    '''
+    for state in ciphertext:
+        # skip special characters
+        if state not in alphabet:
+            plaintext += state
+        else:
+            plaintext += alphabet[ (alphabet.index(state) - key)%25 ] # use mod to keep the alphabet field of 24
 
-    shifted_alphabet = [''] * len(alphabet)
+    return plaintext
 
-    #fill shiftedArr = [3-26]
-    for i in range(key, len(alphabet)):
-        shifted_alphabet[i-key] = alphabet[i]
+def bruteforce(ciphertext):
+    for i in range(1, 25):
+        print(f"{i}: {caesar_decrypt(ciphertext, i)}")
 
-    shifted_alphabet[len(alphabet)-(key):] = alphabet[:key]
-
-    #substitution
-    deciphered = [''] * len(cipher_text)
-    exists = False
-
-    for i in range(len(cipher_text)):
-        for j in range(len(shifted_alphabet)):
-            if cipher_text[i].casefold() == shifted_alphabet[j].casefold():
-                deciphered[i] = alphabet[j]
-                exists = True
-                break
-            else:
-                exists = False
-
-            if not exists:
-                deciphered[i] = cipher_text[i]
-
-    #put the result in a string
-    result = ''
-    for i in range(len(deciphered)):
-        result = result + deciphered[i]
-
-    return result
-
-#----------------------------------------------#
 #---- OUTPUT ----#
-
-#brute-force
-for i in range(1, 26):
-    print(str(i) + ":", end=" ")
-    print(decipher(i, cipher_text_arr))
+bruteforce(ciphertext)
